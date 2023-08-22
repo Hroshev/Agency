450fc93869e26abd1e72dd6cf17ca1b7fc43aacd
Ivan Hroshev <grosheff.ivan@gmail.com>
2023-08-22 17:02:57 +0300

    first commit

--- gulp/tasks/php.js
+++
@@ -1,5 +0,0 @@
-export const php = () => {
-    return app.gulp.src(app.path.src.php)
-        .pipe(app.gulp.dest(app.path.build.html))
-        .pipe(app.plugins.browsersync.stream());
-};
--- src/js/modules/date.js
+++
@@ -1,5 +0,0 @@
-const date = new Date();
-const year = date.getFullYear();
-const copyrightText = document.querySelectorAll('.footer__copyright-text');
-
-copyrightText.forEach(item => item.innerHTML = `<p>© ${year} Trinwdax Digital Agency</p>`)
